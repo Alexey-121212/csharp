20872ebf15ebb40db82bc6707708feea18e727e4
Alexey <alexmail121212@gmail.com>
2023-09-21 16:55:13 +0300

    clear

--- EX3/Program.cs
+++ EX3/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a=7;
-int b=-3;
+int a=new Random().Next(1,10);
+int b=new Random().Next(1,10);
+Console.Clear();
+Console.WriteLine(a);
+Console.WriteLine(b);
 Console.WriteLine(a+b);
